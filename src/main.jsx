6c33d2f0b2401ad70ae6fb4d2cf0b6b7335d3f45
DongGyun <wooheuy5752@gmail.com>
2025-01-13 00:28:47 +0900

    chore: 스타일 컴포넌트 기본 속성 정의 (#1)
    글로벌 스타일
    폰트 설정
    폰트 크기 및 행간 크기 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
 
+import GlobalStyles from './styles/globalStyles.js'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+)
---
+++ src/styles/palette.js
@@ -0,0 +1,30 @@
+const grey = {
+  100: '#FBFBFB',
+  200: '#F2F2F2',
+  300: '#E2E2E2',
+  400: '#BABABA',
+  500: '#9E9E9E',
+  600: '#777777',
+  700: '#3C3C3C',
+}
+
+const blue = {
+  100: '#E2F6FE',
+  200: '#C5EAFE',
+  300: '#A9DCFE',
+  400: '#93CDFD',
+  500: '#6DB0F4',
+  600: '#518ED9',
+  700: '#386AB6',
+}
+
+const ect = {
+  error: '#FF8E5D',
+  success: '#9CDE70',
+}
+
+export const colors = {
+  grey,
+  blue,
+  ect,
+}

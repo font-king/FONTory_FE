85a559fd3078884400004f5814da33e6671a4cf2
DongGyun <wooheuy5752@gmail.com>
2025-01-14 12:33:28 +0900

    feat: 기본 레이아웃 세팅 (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { colors } from './styles/palette'
+
 function App() {
   return (
-    <>
-      <div>home</div>
-    </>
+    <Container>
+      <Header>
+        <h1>
+          <Link to={`/`}>FONTory</Link>
+        </h1>
+      </Header>
+
+      <Main>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Main>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background: ${colors.grey[200]};
+`
+
+const Header = styled.header`
+  background: white;
+  padding: 3.2rem;
+
+  & > h1 {
+    font-family: 'Jalnan';
+    font-size: 3.6rem;
+    color: ${colors.blue[500]};
+  }
+`
+
+const Main = styled.main`
+  flex-grow: 1;
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  gap: 0.8rem;
+  overflow: hidden;
+  padding: 0.8rem;
+`
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import GlobalStyles from './styles/globalStyles.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <GlobalStyles />
-    <App />
+    <BrowserRouter>
+      <GlobalStyles />
+      <App />
+    </BrowserRouter>
   </>,
 )

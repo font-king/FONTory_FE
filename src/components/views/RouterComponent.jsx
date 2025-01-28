09c6200c0d570f9f4556cb8a61b8467a98b54e7b
DongGyun <wooheuy5752@gmail.com>
2025-01-28 13:34:40 +0900

    feat: Bookmark Detail 구현 (#28)

--- src/components/views/RouterComponent.jsx
+++ src/components/views/RouterComponent.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { Bookmark } from '../containers/Bookmark'
+import { BookmarkDetail } from '../containers/BookmarkDetail'
 import { Explorer } from '../containers/Explorer'
 import { ExplorerDetail } from '../containers/ExplorerDetail'
 import { Home } from '../containers/Home'
@@ -12,6 +13,7 @@ export const RouterComponent = () => {
       <Route path="/explorer" element={<Explorer />} />
       <Route path="/explorer/detail/:id" element={<ExplorerDetail />} />
       <Route path="/bookmark" element={<Bookmark />} />
+      <Route path="/bookmark/detail/:id" element={<BookmarkDetail />} />
     </Routes>
   )
 }

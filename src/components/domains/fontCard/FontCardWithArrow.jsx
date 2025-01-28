41c5cf1f05854f09fbacc9b5dc49f3eba7a807bf
DongGyun <wooheuy5752@gmail.com>
2025-01-28 13:21:40 +0900

    feat: FontCardWithArrow 컴포넌트 구현 (#26)

---
+++ src/components/domains/fontCard/FontCardWithArrow.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { CaretRightIcon } from '@/components/views/icons/NonPropIcons'
+
+export const FontCardWithArrow = ({ font, to }) => {
+  return (
+    <LinkContainer to={to}>
+      <FontInfo>
+        <h6 className="font-name">{font.name}</h6>
+        <CaretRightIcon />
+      </FontInfo>
+      <PreviewText>{font.preview}</PreviewText>
+    </LinkContainer>
+  )
+}
+
+const LinkContainer = styled(Link)`
+  ${({ theme }) => theme.flexBox('column', undefined, undefined, '3xl')};
+  ${({ theme }) => theme.borderRadius('md')};
+  ${({ theme }) => theme.padding('lg', 'md', 'sm')};
+  ${({ theme }) => theme.border('divider')};
+  background: white;
+`
+
+const FontInfo = styled.div`
+  ${({ theme }) => theme.flexBox('row', 'center', 'space-between')};
+
+  .font-name {
+    ${({ theme }) => theme.font(400, 'black')};
+  }
+`
+
+const PreviewText = styled.p`
+  ${({ theme }) => theme.font(500, theme.colors.grey[600])};
+  min-height: 10.8rem;
+`

bb2934c168c577622e2aced7e24ddab207dd2df3
DongGyun <wooheuy5752@gmail.com>
2025-02-28 16:18:57 +0900

    feat: 사이드바 컴포넌트명 및 스타일 수정 (#37)

---
+++ src/shared/ui/SideNavigationBar.jsx
@@ -0,0 +1,68 @@
+import { Link, useLocation } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { colors } from '@/app/styles/palette'
+import {
+  NavBookmarkIcon,
+  NavExplorerIcon,
+  NavFontDesignIcon,
+  NavFontSynthesisIcon,
+  NavHomeIcon,
+  NavMyFontIcon,
+} from '@/shared/ui/icons/NavIcons'
+
+const NAV_LIST = [
+  { label: '홈', Icon: NavHomeIcon, url: '/' },
+  { label: '폰트 제작', Icon: NavFontDesignIcon, url: '/font-design' },
+  { label: '폰트 합성', Icon: NavFontSynthesisIcon, url: '/font-synthesis' },
+  { label: '둘러보기', Icon: NavExplorerIcon, url: '/explorer' },
+  { label: '북마크', Icon: NavBookmarkIcon, url: '/bookmark' },
+  { label: '내 폰트', Icon: NavMyFontIcon, url: '/my-fonts' },
+]
+
+export const SideNavigationBar = () => {
+  const { pathname } = useLocation()
+
+  return (
+    <Container>
+      {NAV_LIST.map(({ url, label, Icon }) => {
+        const isActive = url === '/' ? pathname === url : pathname.startsWith(url)
+
+        return (
+          <NavigationItemContainer key={url} to={url} $isActive={isActive}>
+            <Icon isActive={isActive} />
+            <ItemLabel $isActive={isActive}>{label}</ItemLabel>
+          </NavigationItemContainer>
+        )
+      })}
+    </Container>
+  )
+}
+
+const Container = styled.nav`
+  ${({ theme }) => `
+    ${theme.flexBox('column', undefined, 'center', '4xl')}
+    ${theme.padding('sidebar')}
+    ${theme.borderRadius('container')}
+    ${theme.backgroundColor(theme.colors.blue[500])}
+    ${theme.boxShadow('md')}
+  `}
+  height: fit-content;
+`
+
+const NavigationItemContainer = styled(Link)`
+  ${({ theme, $isActive }) => `
+    ${theme.flexBox('row', 'center', undefined, 'xl')}
+    ${theme.padding('lg', 'md')}
+    ${theme.borderRadius('container')}
+    ${theme.backgroundColor($isActive ? '#fff' : colors.blue[500])}
+  `}
+
+  & > svg {
+    flex-shrink: 0;
+  }
+`
+
+const ItemLabel = styled.p`
+  ${({ theme, $isActive }) => theme.font(500, $isActive ? colors.blue[500] : '#fff')};
+`

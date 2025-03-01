b7cea8f0c6375c3c89631b83851c523b0245e936
DongGyun <wooheuy5752@gmail.com>
2025-03-01 23:24:53 +0900

    feat: 폰트 제작 상황 페이지 구현 (#41)

---
+++ src/widgets/font-progress-table/ui/FontProgressTable.jsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components'
+
+const dummyData = [
+  { fontName: '가나다체', createdAt: '2025.03.21', status: '제작 중' },
+  { fontName: '가나다체', createdAt: '2025.03.21', status: '제작 중' },
+  { fontName: '가나다체', createdAt: '2025.03.21', status: '제작 중' },
+  { fontName: '가나다체', createdAt: '2025.03.21', status: '제작 중' },
+  { fontName: '가나다체', createdAt: '2025.03.21', status: '제작 중' },
+  { fontName: '가나다체', createdAt: '2025.03.21', status: '제작 중' },
+]
+
+export const FontProgressTable = () => {
+  const progressData = dummyData
+
+  return (
+    <StyledTable>
+      <thead>
+        <tr>
+          <th>폰트 이름</th>
+          <th>제작 일자</th>
+          <th>제작 상황</th>
+        </tr>
+      </thead>
+      <tbody>
+        {progressData.map(({ fontName, createdAt, status }, index) => (
+          <tr key={index}>
+            <td>{fontName}</td>
+            <td>{createdAt}</td>
+            <td>{status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </StyledTable>
+  )
+}
+
+const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  text-align: center;
+
+  th,
+  td {
+    ${({ theme }) => theme.padding('sm')};
+    border: 1px solid #ddd;
+  }
+
+  th {
+    ${({ theme }) => theme.font(800, theme.colors.grey[200])};
+    background-color: ${({ theme }) => theme.colors.grey[600]};
+  }
+
+  td {
+    ${({ theme }) => theme.font(800, theme.colors.grey[600])};
+  }
+
+  tr:nth-child(even) {
+    background-color: ${({ theme }) => theme.colors.grey[200]};
+  }
+
+  tr:hover {
+    background-color: ${({ theme }) => theme.colors.grey[300]};
+  }
+`

cca4c8fba9f45b7bbe17c723af823a4de406a4bf
DongGyun <wooheuy5752@gmail.com>
2025-03-17 17:05:03 +0900

    feat: 개발용 토큰 적용 (#49)

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import HttpClient from './HttpClient'
 
 const BASE_URL = import.meta.env.VITE_PUBLIC_SERVER_DOMAIN
+const ETC_CODE = import.meta.env.VITE_PUBLIC_ETC
 
 export const instanceWithoutAuth = axios.create({
   baseURL: BASE_URL,
@@ -16,6 +17,7 @@ export const instance = new HttpClient({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    Authentication: ETC_CODE,
   },
   withCredentials: true,
 })

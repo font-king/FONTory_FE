e785efc56ed38314bf59f933223568f2edf71fe5
DongGyun <wooheuy5752@gmail.com>
2025-03-25 14:03:01 +0900

    feat: 북마크한 폰트 보기 구현 (#59)

--- src/widgets/font-list/BookmarkFontList.jsx
+++ src/widgets/font-list/BookmarkFontList.jsx
@@ -1,31 +1,26 @@
+import InfiniteScroll from 'react-infinite-scroller'
+
 import { GridContainer } from '@/app/styles/commonStyle'
+import { useFetchBookmarkFontList } from '@/entities/font/api/useFont.query'
+import { useSearchKeyword } from '@/features/search/model/search.store'
 import { EmptyMessage } from '@/shared/ui/EmptyMessage'
 
 import { FontItemWithProfile } from './item/FontItemWithProfile'
 
-const dummyFonts = () => {
-  return Array.from({ length: 8 }, (_, i) => ({
-    id: i,
-    fontName: `가나다체 ${i}`,
-    authorName: '고로케',
-    preview:
-      i % 2 === 0
-        ? '동해물과 백두산이 마르고 닳도록 하느님이 보우하사'
-        : '동해물과 백두산이 마르고 닳도록 하느님이 보우하사닳도록 하느님이 보우하사',
-    isBookmarked: i % 2 === 0,
-  }))
-}
-
 export const BookmarkFontList = () => {
-  const fontList = dummyFonts()
+  const searchKeyword = useSearchKeyword()
+
+  const { data: fontList, hasNextPage, fetchNextPage } = useFetchBookmarkFontList(searchKeyword)
 
   if (!fontList || !fontList.length) return <EmptyMessage message="아직 등록된 폰트가 없습니다." />
 
   return (
-    <GridContainer $columns="1fr 1fr" $gap="lg">
-      {fontList.map((font) => (
-        <FontItemWithProfile key={font.id} font={font} />
-      ))}
-    </GridContainer>
+    <InfiniteScroll hasMore={hasNextPage} threshold={250} loadMore={() => fetchNextPage()}>
+      <GridContainer $columns="minmax(0, 1fr) minmax(0, 1fr)" $gap="lg">
+        {fontList.map((font) => (
+          <FontItemWithProfile key={font.id} font={font} />
+        ))}
+      </GridContainer>
+    </InfiniteScroll>
   )
 }
